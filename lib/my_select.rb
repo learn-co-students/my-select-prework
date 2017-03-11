fd4728cde942b3f3841877d97e4c5077b43d1c19
Maxwell Benton <maxwellbenton@gmail.com>
2017-03-11 21:07:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
-def my_select(collection)
+def my_select(collection = [])
  # your code here!
+ i = 0
+ arr = []
+ if collection != []
+   while i < collection.length
+     value = yield(collection[i])
+     if value == true
+       arr << collection[i]
+     end
+     i += 1
+   end
+ else
+ end
+  arr
+end
+
+my_select(collection = []) do |e|
+  if e.even?
+    e
+  end
 end
