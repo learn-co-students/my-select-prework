afed25ee9abe7dbca1b75292bb9cb679d98dea3e
Matthew Hung <matthewbhung101@gmail.com>
2017-03-30 21:49:01 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
-end
+    i = 0
+    mod = []
+    while i < collection.length
+      new_var = yield collection[i]
+      if (new_var == true)
+      mod.push(collection[i])
+      end
+      i = i + 1
+    end
+    mod # put argument(s) here# code here
+  end
