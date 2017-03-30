66fff5ec21a07ace760a0481587efe1c5ff1e9e0
Gaby Stern <stern.gabrielle@gmail.com>
2017-03-30 14:53:04 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
-def my_select(collection)
- # your code here!
+nums = [1, 2, 3, 4, 5]
+
+def my_select (array)
+  i = 0
+  new_array = []
+  if array.length == 0
+    "This block should not run!"
+  else while i < array.length
+    if yield(array[i]) == true
+      new_array.push(array[i])
+    end
+    i = i+1
+    end
+  end
+  new_array
 end
+
+#my_select(nums) do |number|
+  #if array[i].even?
+    #number
+#end
