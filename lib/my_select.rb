96d9eaeea5ab62ff92a04689dedd79b425250028
Alexandra Williams <alexandrawilliams.aiw@gmail.com>
2017-04-02 01:41:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
+nums = [1, 2, 3, 4, 5]
+
 def my_select(collection)
  # your code here!
+ counter = 0
+ empty_array = []
+ while counter < collection.length
+  if collection[counter].even?
+     #return collection[counter]
+    # empty_array.push(yield collection[counter])
+    empty_array.push(collection[counter])
+   end
+ counter += 1
+ end
+ empty_array
 end
+
+
+my_select(nums) {|num| return num }
