94f22cf748c22bc062337205979e4a159729fd00
Chris Dascoli <chris.dascoli@gmail.com>
2017-04-07 14:29:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
+
+#require 'pry'
 def my_select(collection)
- # your code here!
+  storage = []
+  i = 0
+
+  while i < collection.length
+    if yield(collection[i]) == true
+      storage <<
+      i += 1
+    elsif yield(collection[i]) == false
+      i += 1
+    end
+  end
+  storage
+end
+
+nums = [1, 2, 3, 4, 5]
+
+my_select(nums) do |num|
+  num.even?
 end
