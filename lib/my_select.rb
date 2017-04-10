7628ba04628f156a89c5f0ae1c38e80911062649
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-04-09 20:55:39 -0500

    ＴＨＩＳ 　ＭＵＳＴ 　ＢＥ 　ＴＨＥ 　ＷＯＲＫ 　ＯＦ 　ＡＮ 　ＥＮＥＭＹ 「ＳＴＡＮＤ」！！

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+ if block_given?
+   array_to_return = []
+   i = 0
+   while i < collection.length
+     result = yield collection[i]
+     if result == true
+       array_to_return.push(collection[i])
+     end
+     i += 1
+   end
+   array_to_return
+ else
+   nil
+ end
 end
