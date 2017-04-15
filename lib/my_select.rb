0896774380b74ed460f60b15630a1f6a4cd6130a
Vivian Lin <lin.vvian@gmail.com>
2017-04-15 19:05:19 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ ret = []
+ if collection.any? == false
+   puts "empty"
+ else
+   if block_given?
+     while i<collection.length
+      if yield(collection[i]) == true
+       ret << collection[i]
+      end
+      i += 1
+     end
+   end
+ end
+ ret
 end
+
