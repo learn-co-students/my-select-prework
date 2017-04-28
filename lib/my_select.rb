3450b211ba340c193980142f8a305aea73244a5d
Tyler Knight <tknightx31@gmail.com>
2017-04-28 02:27:51 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  index = 0
+  return_array = []
+  while index < array.length
+    if yield array[index]
+      return_array << array[index]
+    end
+    index += 1
+  end
+
+  return_array
+
 end
