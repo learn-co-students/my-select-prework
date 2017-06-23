382fd60df1e411a268855f8138756efed41fabac
Martina Weidenbaum <martina.weidenbaum@gmail.com>
2017-06-23 01:55:42 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ new_array = []
+ while i < collection.length
+   if yield(collection[i]) == true
+     new_array << collection[i]
+   end
+   i = i + 1
+ end
+ new_array
+end
+
+my_select([]) do |i|
+  i.even?
 end
