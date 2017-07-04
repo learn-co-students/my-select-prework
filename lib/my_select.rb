f78323081fc4cb975419f3e0cb03233c276e8e3b
joshlacey <williamjoshualacey@gmail.com>
2017-07-04 00:59:12 -0400

    done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+    new_array = []
+    for element in collection
+        if yield(element)
+        new_array << element
+        end
+    end
+    new_array
 end
