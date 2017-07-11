b1b72fd7d2bbfd6d7d61b473c8d591ce58421377
bleavs <bleavitt@colgate.edu>
2017-07-11 12:46:43 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+ new_arr = []
+ 
+ i = 0
+ 
+    while i < collection.length
+        if yield(collection[i])
+            new_arr.push(collection[i])
+        end
+        i += 1
+    end
+ 
+    new_arr
 end
