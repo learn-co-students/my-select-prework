771f9d430c94701652ee01e20c6048f226a475e7
Charlie Max <dherzl@protonmail.com>
2017-08-01 22:24:25 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,26 @@
-def my_select(collection)
- # your code here!
+def my_select(arr)
+
+  i = 1
+  new_arr = []
+
+  while i < arr.length
+      if arr[i].even?
+        yielded = arr[i]
+      else
+        new_arr << yielded
+      end
+        i = i + 1
+
+  end
+  new_arr
 end
+
+
+
+
+
+# it "selects the items in the collection to which the block returns true" do
+#   expect(my_select(nums) do |num|
+#     num.even?
+#   end).to eq([2, 4])
+# end
