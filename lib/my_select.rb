2ea8bf6b066c977f90ee7c571b8da6473fdb5267
Al Artesona <artesonaa2@gmail.com>
2017-08-04 01:28:56 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+ empty_array = []
+ answer_array = []
+ while counter < collection.length
+   empty_array.push(yield collection[counter])
+   if empty_array[counter] == true
+     answer_array.push(collection[counter])
+   end
+   counter += 1
+ end
+ answer_array
 end
