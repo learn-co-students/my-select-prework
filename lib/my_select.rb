5db41b0d797ad4eb60123c665f990ad76dc9d964
William Scripps <wlscripps@gmail.com>
2017-08-11 05:48:13 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,18 @@
-def my_select(collection)
- if block_given?
-   result = collection.select {|x| x.even?}
-   return result
- else
-   puts "No block!"
- end
-
+def my_select(array)
+  if block_given?
+  i = 0
+  result = []
+  while i < array.length
+    if yield(array[i])
+      result.push(array[i])
+    end
+    i += 1
+  end
+  result
+end
 end
 
 
 #my_select(array) do |num|
-#  num
+#  num.even?
 #end
