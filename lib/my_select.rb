ce561cc6de572c36d63da1631d8d55f8757d36d6
William Scripps <wlscripps@gmail.com>
2017-08-11 05:43:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+ if block_given?
+   result = collection.select {|x| x.even?}
+   return result
+ else
+   puts "No block!"
+ end
+
 end
+
+
+#my_select(array) do |num|
+#  num
+#end
