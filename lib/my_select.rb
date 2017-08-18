b38006ab7957a36ed145abff49ba2205a6ccafa4
Ayreal <aeh.herman@gmail.com>
2017-08-18 17:52:52 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  #[1,2,3,4,5].select { |num|  num.even?  }   #=> [2, 4]
+  # iterates over the collection, returns a new collection of elements for which the code in the block is true
+    i = 0
+    new_array = []
+
+    while i < collection.length
+      var = yield(collection[i])
+      if var == true
+        new_array.push(collection[i])
+      end
+      i = i + 1
+    end
+
+    new_array
+
 end
