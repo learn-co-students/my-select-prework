8df4d47bdb02a97a7244a2cfe8717cfce97587ce
gene-yoo <gene.y.yoo@gmail.com>
2017-08-24 17:06:20 -0400

    Lab complete.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  solution = []
+  while i < collection.length
+    if (yield collection[i])
+      solution << collection[i]
+    end
+    i += 1
+  end
+  solution
 end
