fbdfbe19d72642a392c37f71b7a9a48bbc064e51
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-25 10:07:42 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,20 @@
 def my_select(array)
-  i = 0
-  arrays = []
-  while i < array.size
-    if yield(array[i]) == true
-      arrays << array[i]
-    else
-    end
+
+    arrays = []
+
+    #----
+
+    i = 0
+    while i < array.size
+
+        if yield(array[i]) == true
+
+            arrays << array[i]
+
+        end
+
     i += 1
-  end
-  arrays
+
+    end
+    return arrays
 end
