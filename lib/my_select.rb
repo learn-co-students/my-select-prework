c2068fd09bf5ee0d1b12d188f3d009832f93602a
Dick Ward <dick@thedickward.com>
2017-09-03 16:50:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(collect)
+  i = 0
+  new_array = []
+  while i < collect.length
+    if yield(collect[i]) == true
+      new_array << collect[i]
+    end
+    i += 1
+  end
+  new_array
 end
