3791c1f952b473ebc7a13bfd0e8e84ff3a57c68b
Shirley Lin <s.lin12@gmail.com>
2017-09-13 00:47:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+ x = 0
+ selected = []
+ while x < collection.length
+   selected << collection[x] if yield(collection[x]) == true
+   x += 1
+ end
+selected
 end
