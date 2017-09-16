055db60fd3c9c3a505c4b571de27d8e8c5bc50df
MatteBru <mbruni616@gmail.com>
2017-09-16 10:03:41 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  sel = []
+  i = 0
+  while i < collection.size
+    sel << collection.at(i) if (yield collection.at(i)) == true
+    i += 1
+  end
+  sel
 end
