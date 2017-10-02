126674a0460fab273aeb2761cc560d69b3d4061e
Ellisa Shim <ellishim@gmail.com>
2017-10-02 17:17:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
+  new_array = []
+  i = 0
+  while i < collection.length
+    yield collection[i]
+      if (yield collection[i]) == true
+        new_array.push(collection[i])
+        i += 1
+      else
+        i += 1
+      end
+    end
+  new_array
  # your code here!
 end
