47da7e0cf81542b017b1173d0aeeaeabf2005d37
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-10-22 20:07:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0
+   new_array = []
+   while i < collection.length
+     if yield collection[i]
+       new_array.push(collection[i])
+     end
+     i += 1
+   end
+   puts new_array
+   new_array
+ else
+   "No Block Was Given!"
+ end
+end
+
+collection = (1..5).to_a
+
+my_select(collection) do |number|
+  number.even?
 end
