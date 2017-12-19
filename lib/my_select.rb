70d8fae5f63d9bd538bae8f211a5fe2ea3ddf15d
Dwire <gjdwyer8@gmail.com>
2017-12-19 14:14:33 -0500

    Complete my-select prework

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    return_array = []
+    while i < collection.length
+      return_array << collection[i] if yield(collection[i]) == true
+      i += 1
+    end
+    return_array
+  else
+    "Feed me a block"
+  end
 end
