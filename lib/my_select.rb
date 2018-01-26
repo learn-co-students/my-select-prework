13664eb7abae80d2e751c2be1b9380865d994044
Helen Liutongco <liutongcoh@gmail.com>
2018-01-25 23:17:56 -0600

    Modify my_select.rb

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  new_collection = Array.new
+  while counter < collection.length
+    if yield(collection[counter])
+      new_collection << collection[counter]
+    end
+    counter += 1
+  end
+  new_collection
 end
