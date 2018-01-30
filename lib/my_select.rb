5b8c42f09bfce1f08fa79767aec404c1475d2c7d
github username <github email address>
2018-01-29 19:38:52 -0500

    I made my own select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ if collection.length > 0
+   i = 0
+   new_collection = []
+   while i < collection.length
+     if yield collection[i]
+       new_collection.push(collection[i])
+     end
+     i += 1
+   end
+ end
+ return new_collection 
 end
