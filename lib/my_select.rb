af07a4bccdd15f6f55b4304b1b79aeee23b7937c
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-01 17:05:03 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  collection1 = Array.new
+  while i < collection.length
+    temp = yield(collection[i])
+    if temp == true
+      collection1.push(collection[i])
+    end
+    i = i + 1
+  end
+  collection1
 end
