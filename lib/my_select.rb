c47e3f14eb635ebe80a098c53dd5e012e89e474d
victoria-huang <vctrah@gmail.com>
2018-02-09 21:57:50 -0500

    select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+ new_collection = []
+ i = 0
+ 
+ while i < collection.length
+     ele = collection[i]
+     
+     if (yield ele)
+         new_collection << ele
+     end
+     
+     i += 1
+ end
+ 
+ new_collection
 end
