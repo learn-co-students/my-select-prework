c464e67badc5b64c988544b81dee18177114a1d5
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-12 17:05:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ out = []
+ while i < collection.length do
+   if yield(collection[i])
+     out << collection[i]
+   end
+   i += 1
+ end
+ out
 end
