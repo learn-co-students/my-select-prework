3bd804e89ff3959baa4f840837c13ffa061d4100
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-10 00:54:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if array == []
+    return "This block should not run!"
+  end
+  
+  if block_given?
+    output = []
+    i = 0
+    while i < array.length
+      if yield(array[i]) == true
+        output.push(array[i])
+      end
+      i = i + 1
+    end
+    output
+  end
 end
