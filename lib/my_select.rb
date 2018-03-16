bd3ffc98346b7f77e6c7b1a24977532a712eb1af
briecodes <hello@brie.work>
2018-03-16 16:31:44 -0400

    My Select method created.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  new_array = []
+  i = 0
+  while i < collection.size
+    if yield(collection[i])
+      new_array << collection[i]
+      i += 1
+    else
+      i += 1
+    end
+  end
+  new_array
 end
