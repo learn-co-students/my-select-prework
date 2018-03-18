d96f9f7ef8274b74fffb6999fc6a07d7559a37ba
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-18 00:34:03 +0000

    first commit

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+ new_collection = []
+ idx = 0
+
+ while idx < collection.length
+   result = yield collection[idx]
+
+   if result == true
+     new_collection << collection[idx]
+   end
+
+   idx += 1
+ end
+
+ new_collection
 end
