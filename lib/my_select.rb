cc61af29d7cadc1d54601cb01eb28aafa5e5549f
Michelle Blevins <mjblevins7@gmail.com>
2018-03-24 22:06:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    new_collection = []
+
+    while i < collection.length
+      if yield(collection[i]) == true
+      new_collection << collection[i]
+        i = i + 1
+      else
+        i = i + 1
+      end
+    end
+
+    new_collection
+  else
+    "No block was given!"
+  end
 end
