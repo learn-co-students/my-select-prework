0078f6b0687fe5fde52e9e63d7e889e7cce3c408
ensallee <ensallee@gmail.com>
2018-03-27 15:51:48 -0400

    select with yield

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ new_collection=[]
+ if block_given?
+   i=0
+   while i < collection.length
+     if yield(collection[i]) == true
+       new_collection << collection[i]
+     end
+     i+=1
+   end
+   new_collection
+ else
+   "No block was given"
+ end
 end
