43a3e739af245a3b131f3e949ae13eebcf8f9a47
travisdock@gmail.com <travisdock@gmail.com>
2018-04-04 10:36:18 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_collection = []
+  if collection.length > 0
+    while i < collection.length
+      if yield(collection[i]) == true
+        new_collection.push(collection[i])
+      end
+      i += 1
+    end
+    new_collection
+  else
+    puts "You messed up, dawg. Try again."
+  end
 end
