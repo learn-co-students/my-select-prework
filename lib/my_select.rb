cac7ee4be9fcf4ef0481b481908b4ab2830df7f4
joshpson <jpearson846@gmail.com>
2018-04-08 17:53:57 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i = 0
+  selected = []
+  if array.length > 0
+    while i < array.length
+      item_test = yield array[i]
+      if item_test == true
+        selected << array[i]
+      end
+      i += 1
+    end
+  else
+    "Pass an array with items!"
+  end
+  selected
 end
