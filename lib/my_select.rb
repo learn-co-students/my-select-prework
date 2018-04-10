0410485774ad04aaa42a2c8f27b07a10920edd5f
Saige Leslie <saigeleslie@gmail.com>
2018-04-10 17:53:00 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new_collection = []
+  
+  if collection.length > 0 
+    while i < collection.length 
+      if yield(collection[i])
+      new_collection << collection[i]
+      end
+      i += 1 
+    end
+  else
+    puts "There is no value!"
+  end
+  
+  new_collection
 end
