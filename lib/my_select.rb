92108c56ec12cc46eb9c553f2c13a90ddf05bb85
Canino Developments <ian.canino@gmail.com>
2018-04-15 15:44:51 -0400

    implemented a variant of the array.select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  index = 0
+  truthy = []
+    while index < collection.length
+      if yield(collection[index])
+      truthy << collection[index]
+      index += 1
+      else
+        index += 1
+      end
+    end
+    truthy
 end
