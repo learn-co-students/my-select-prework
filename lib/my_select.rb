9146f53c63e18129806e3341e49847c6c4524aaf
JohnDaise <jdaise@gmail.com>
2018-05-11 09:39:50 -0400

    first commit

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0
+   select = []
+
+   while i < collection.length
+     if yield(collection[i]) == true
+       select << collection[i]
+     else
+
+     end
+     i+=1
+   end
+    select
+  else
+    collection
+  end
 end
