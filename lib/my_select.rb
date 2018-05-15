e6408dc5e0ec84890405921c63ec11486e0b1584
github username <github email address>
2018-05-14 20:48:31 -0700

    took out the extra condition in if statement

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,8 +3,8 @@ def my_select(collection)
   new_collection = []
 
   while i < collection.length
-    if yield(collection[i])==true
-      new_collection.push(yield(collection[i]).values)
+    if yield(collection[i])
+      new_collection.push(yield(collection[i]))
       i += 1
     else
       i += 1
