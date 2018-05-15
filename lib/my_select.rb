6827993a5694836a4eb9092710df8c77cf6161a7
github username <github email address>
2018-05-14 21:06:50 -0700

    removed yield keyword in body of conditional

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
 
   while i < collection.length
     if yield(collection[i])
-      new_collection.push(yield(collection[i]))
+      new_collection.push(collection[i])
       i += 1
     else
       i += 1
