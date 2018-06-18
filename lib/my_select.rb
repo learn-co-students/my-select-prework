eeafc06998e8247d5c5f122e9d4c0a9245c72cc2
Rebecca Govert <becci.govert@gmail.com>
2018-06-18 20:35:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
+
 def my_select(collection)
- # your code here!
+ results_array = []
+ if block_given?
+   i = 0 
+   while i < collection.length 
+     if yield collection[i] 
+       results_array << collection[i]
+      end
+      i += 1
+    end
+    results_array
+  else
+    "No block_given"
+  end
 end
+
+numbers = [1,2,3,4,5]
+
+my_select(numbers) do |num|
+ num % 2 == 0
+end
+
