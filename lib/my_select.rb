88c1695e0b0878bed8c565b800de0d6da66d6ebc
whynotski <laurawynosky@gmail.com>
2018-06-18 11:21:41 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,33 @@
-def my_select(collection)
- # your code here!
+# def my_select(collection)
+#  # your code here!
+#   empty_array=[]
+#
+#   i=0
+#   while i < collection.length
+#     empty_array << yield(collecion[i])
+#     i += 1
+#   end
+#
+#   empty_array.compact
+# end
+#
+# my_select(nums) do |num|
+#   if num.even?
+#     num
+#   end
+# end
+
+#nums = [1, 2, 3, 4, 5]
+
+  def my_select(array)
+    i =0
+    select=[]
+    while i < array.length
+      if yield(array[i])
+        select << array[i]
+      end
+      i += 1
+    end
+  select
 end
+#my_select(nums)
