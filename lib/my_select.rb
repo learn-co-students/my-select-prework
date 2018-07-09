642783d651b4f985b58bcb8b13f009a6310aecd6
Kristopher <kris.bullinger@gmx.com>
2018-07-09 19:39:45 -0400

    Lab Complete

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  select_array = []
+ i = 0
+ while i < collection.length
+   select_array.push(collection[i]) if (yield collection[i])
+   i += 1
+ end
+ select_array
 end
