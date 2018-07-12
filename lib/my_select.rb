42fe0552aaafb19302e947acca5d0cabecfaa0c5
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-12 20:39:54 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  counter = 0
+  newer_array = []
+  while counter < array.length
+
+
+    if yield(array[counter]) == true
+      newer_array << array[counter]
+      counter += 1
+    else
+      counter += 1
+    end
+
+  end
+
+  return newer_array
 end
