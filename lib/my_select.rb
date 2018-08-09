345ad2be1d317643941fee7e61840c9b8709403a
Jacob Berman <jacobberman1995@gmail.com>
2018-08-09 03:12:56 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ i = 0 
+ sorted = []
+ while i < collection.length
+  if !yield(i) 
+    sorted.push(collection[i])
+  end
+  i += 1
+end
+sorted
 end
