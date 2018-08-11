3cab9c6c71e7c70e7eee93e667f065d10fc04f05
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-11 16:21:39 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while i < collection.length 
     yield collection[i] 
       if yield collection[i]
-       new_array << i
+        new_array << i
       end 
     i += 1
   end 
