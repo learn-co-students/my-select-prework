f906c938057870103ef41b9cf873dcf76bbc60f2
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 23:03:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
 def my_select(collection)
- # your code here!
+  if collection.length == 0
+    puts "This block should not run"
+  else
+    i = 0
+    new_collection = []
+    
+    while i < collection.length
+      if yield collection[i]
+        new_collection.push(collection[i])
+      end
+      i += 1
+    end
+    
+    new_collection
+  end
+end
+
+nums = [1, 2, 3, 4, 5]
+my_select(nums) do |num|
+  num % 2 == 0
 end
