323934d4b7df2b7f9dc3cb5d68c130d0cd824ff9
github username <github email address>
2018-09-04 16:46:40 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
 def my_select(collection)
  # your code here!
+  i = 0
+  selection = []
+  while i < collection.length
+    #how can i check if the block condition will evaluate to true?
+    if yield(collection[i]) == true
+       selection << yield(collection[i])
+     end
+    i += 1
+  end
+  selection
 end
+
+#the while i<collection.length makes sure the method goes through the whole
+#parameter array.
+#the selection << yield(collection[i]) if block_given? is currently pushing
+#all the yielded values into the empty selection array but i need a condition
+#to filter out the yield values that are "false"
+#if the yield block evaluates to true, then push/shovel the yield value
+#to the empty array - how do i check if the yield block condition will
+#evaluate to true?
