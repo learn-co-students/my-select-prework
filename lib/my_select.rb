7656dafcc5060406d530d6811126457ff60b9136
Eric Klimowich <you@example.com>
2018-09-04 12:42:00 -0400

    Select at last

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ new_collection = []
+ while i < collection.length
+   new_collection.push collection[i] if (yield collection[i])
+   i += 1
+ end
+ new_collection
 end
