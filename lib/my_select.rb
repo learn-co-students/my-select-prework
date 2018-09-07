9be65cadeee7cdef099e77cd812cec1af544e770
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-07 17:06:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  if block_given? == false
+    return array
+  end
+  i=0
+  new_array=[]
+  while i<=array.size-1
+   new_array.push(array[i]) if  yield array[i]
+    i+=1
+  end
+  new_array
 end
+
