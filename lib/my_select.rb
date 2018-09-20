02d265f50b5aa0b54f90f29ce261eb158663ae16
Irfan Suleman <ivsuleman@gmail.com>
2018-09-20 16:40:58 +0100

    fixed

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,32 @@
-def my_select(collection)
+require 'pry'
+
+def my_select(arr)
  # your code here!
+    i = 0
+    evens_arr = Array.new
+
+    while i < arr.length
+        if yield(arr[i])
+            evens_arr << arr[i]
+        end
+        i += 1
+    end
+    evens_arr
 end
+
+my_select([1, 2, 3, 4, 5]) do |num|
+    num.even?
+end
+
+
+def my_select(array)
+    i = 0
+    select = []
+    while i < array.length
+      if yield(array[i])
+        select << array[i]
+      end
+      i+=1
+    end
+    select
+  end
