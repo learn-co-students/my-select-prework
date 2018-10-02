2b068ec454c211dde237cebb251e4f8c6c9c760f
Jacob Menashi <jbmenashi@gmail.com>
2018-10-02 18:47:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ if collection.length < 1 
+   return false
+ else
+   i = 0 
+   final_array = []
+   while i < collection.length
+     if yield(collection[i])
+        final_array << collection[i]
+      end
+     i += 1
+   end
+   final_array
+ end
 end
