8644aabd6181479b7a32764668be7e42f5becbcc
Tom Rowson <tom.rowson@gmail.com>
2018-10-22 13:11:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,7 @@
-def my_select(collection)
- # your code here!
-end
+ def my_select(array, &block)
+    result =[]
+    array.each do |element|
+      result << element if block.call(element) == true
+    end
+    result
+  end
