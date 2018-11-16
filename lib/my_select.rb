233a172eafeb1e8c95b375d82407ef1130f8fca5
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-16 17:35:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
-end
+  counter = 0 
+  new_collection = []
+  while collection.length > counter 
+    if yield(collection[counter]) == true
+      new_collection << collection[counter]
+    end 
+    counter += 1 
+  end 
+  new_collection
+end 
