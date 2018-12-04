3744d7d5bb490706286ee2b70c82301f2d795f52
Nison Malayev <nmalayev@gmail.com>
2018-12-04 01:55:28 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+  if collection.length == 0
+    "Looks like your collection is empty."
+    
+  else
+    i = 0
+    new_ary = []
+    
+    while i < collection.length
+      if yield(collection[i]) == true
+        new_ary << collection[i]
+      end
+      i += 1
+    end
+    
+    new_ary
+  end
 end
