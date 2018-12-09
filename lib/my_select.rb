7b564fdba95f37014f5e6aa3a7ed94245f7e6d81
generous-program-0200 <harrycopley@vultuslimited.com>
2018-12-09 00:27:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+i = 0
+new_list = []
+while i < collection.length
+if yield(collection[i])
+  new_list << collection[i]
 end
+i += 1 
+end
+new_list
+end
+
