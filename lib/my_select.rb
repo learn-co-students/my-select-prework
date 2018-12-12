7e2eb0f30654d8abfc9ca0b7fa6bde9437cd25c3
Jack Rotta <jackrotta12@gmail.com>
2018-12-12 23:37:02 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
+#common learn submit plzzzz
 def my_select(collection)
- # your code here!
+ counter =0
+ newCollect = []
+ while counter < collection.size
+   if yield collection[counter]
+     newCollect << collection[counter]
+   end
+   counter+=1
+ end
+ return newCollect
 end
