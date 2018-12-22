8cd5fe06105ab74b98551d38fd85d66089760683
mkay0961 <mkay0961@gmail.com>
2018-12-22 16:26:38 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  if collection.length == 0
+    return collection
+  else
+    i=0
+    array = []
+    while i<collection.length do
+      if (yield(collection[i]) == true)
+        array.push(collection[i])
+      end
+      i+=1
+    end
+    return array
+  end
+
+
 end
