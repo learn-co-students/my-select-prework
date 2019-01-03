2154b385ff791f717da42c72c31ceac0492a9747
github username <github email address>
2019-01-03 16:03:58 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
- # your code here!
+ if collection.length > 0
+   counter =  0
+   new_collect = []
+
+   while counter < collection.length
+     if collection[counter].even?
+       new_collect.push(collection[counter])
+     end
+     counter += 1
+   end
+
+ else
+   return "This block should not run!"
+ end
+
+ new_collect
 end
