3a8fbfb534414e72be4211b9377df75d437f3183
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-23 19:06:25 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ selectiveArray = []
+ i = 0
+ while i < array.length
+ if yield(array[i])
+   selectiveArray.push(array[i])
+ end
+ i += 1
+ end
+ selectiveArray
 end
