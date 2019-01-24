23f92eb450b16096cd6d9a61cfc6b6d2fcd2312c
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-01-23 21:52:28 -0600

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i=0
+  result =[]
+  while i<collection.length
+    yield collection[i]
+    result << collection[i] if collection[i].even?
+    i+=1
+  end
+  result
+
 end
