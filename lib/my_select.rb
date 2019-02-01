8e95104362c50cf8fb2c5c90240bd636205db7e3
Noah Fairbairn <noahfairbairn@gmail.com>
2019-02-01 19:22:00 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def my_select(collection)
  # your code here!
+ i = 0
+ new_arry = []
+ while i < collection.length
+   if yield(collection[i]) == true
+     new_arry << (collection[i])
+   end
+   i+=1
+ end
+ new_arry
+ binding.pry
 end
