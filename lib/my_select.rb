13e79b82d049e1600cc2cbd950489f358683ecb7
ko kristin <koberlander115@gmail.com>
2019-02-10 01:56:16 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,11 @@
 def my_select(collection)
- collection.each do |i| 
-  newCollection << yield([i])
+  i = 0
+  newCollection = []
+  while i < collection.length
+    if yield(collection[i])
+      newCollection << collection[i]
+    end
   i += 1
 end
+  newCollection
 end
