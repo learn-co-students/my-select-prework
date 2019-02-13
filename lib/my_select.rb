d7791c632fff1cee803eca9675ff0ea5a2d60ca9
Ben Manekin <ben.manekin@gmail.com>
2019-02-13 00:52:45 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,11 @@
 def my_select(collection)
   new_list = []
+  i = 0
   for num in collection
-    new_list << num if num % 2 == 0
+    if yield(collection[i]) == true
+      new_list << num
+    end
+    i += 1
   end
   new_list
 end
