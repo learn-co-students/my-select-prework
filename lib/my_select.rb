d6f72075ebc6911fe33892c373620f01a4023a1c
github username <github email address>
2019-04-19 17:40:06 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,49 @@
-def my_select(collection)
-  collection.select { |x| x.even?}
+def my_select(array)
+  array.select {|i| i.even?}
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def my_select(collection)
+#   collection.select { |x| x.even?}
+# end
