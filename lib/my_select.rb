c7a9168a1c53704ba4ebbb565ce465580c22b814
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-04-24 12:15:15 +0800

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i=0
+  array2=[]
+  while i<collection.length
+    if yield(collection[i])
+      array2.push(collection[i])
+    end
+    i+=1
+  end
+  array2
 end
