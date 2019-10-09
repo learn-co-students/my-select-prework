53fa8a53dd9b612d297327a4a7b30532d5d47382
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:59:19 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    array = []
+    while i < collection.size do
+      if yield(collection[i]) 
+        array << collection[i]
+      end
+      i += 1
+    end
+    
+    array
+  end 
 end
+
+collection = [1,2,3,4,5]
+
+my_select(collection) {|element|
+  element.even?
+}
