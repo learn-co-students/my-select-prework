ddf1135623fae27b3b706ebc111002ef2416fac4
dagonyclark <sean@dagony-clark.com>
2019-11-20 10:08:32 -0500

    Revert "Done."
    This reverts commit 5bc9858d097bcee05fbf2769d41941ec062fae1c.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,19 +1,3 @@
 def my_select(collection)
-  counter = 0
-  collection_truth = []
-  collection_select = []
-  while counter < collection.length
-    collection_truth[counter] = yield collection[counter]
-    counter += 1
-  end
-  counter = 0
-  counter_truth = 0
-  while counter < collection.length
-    if collection_truth[counter] == true
-      collection_select[counter_truth] = collection[counter]
-      counter_truth += 1
-    end
-    counter += 1
-  end
-  collection_select
+ # your code here!
 end
